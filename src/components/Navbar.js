4fe798dc15bb2a3add3c52c091ad9f6660faa546
Rishabh Sinha <rishabh210798@gmail.com>
2022-12-17 20:50:50 +0530

    project setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <h1>ML</h1>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
