280010311fd15af9daa31923d2e3658e70d2ae44
Garima Kaur <kaurgarima.284@gmail.com>
2022-12-19 23:42:28 +0530

    homepage,css

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 const bgImg = new URL("../../public/images/ML.jpg", import.meta.url);
-
 const HomePage = () => {
   return (
-    <>
-      <div className="home-container">
-        <img className="bgImg" src={bgImg} alt="BgImage"></img>
-        <Link to="/know-more">
-          <button>Click Me</button>
-        </Link>
+      <div>
+        <div className="img-container"></div>
+        <div className="main-text-box">
+          <h1>DIABETES PREDICTION</h1>
+          <Stack spacing={2} direction=" row" className="stack-mui">
+          <Link to="/know-more">
+            <Button class="button-km" variant="contained">
+              HomeScreen
+            </Button>{" "}
+          </Link>
+          <Link to="/">
+            <Button class="button-km" variant="contained">
+              Next Page
+            </Button>
+          </Link>
+        </Stack>
       </div>
-    </>
+    </div>
   );
 };
 
