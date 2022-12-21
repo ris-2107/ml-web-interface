24e78a9b070584d15018917fe290590b4ce27104
Garima Kaur <kaurgarima.284@gmail.com>
2022-12-21 22:43:28 +0530

    hi-ml

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import KnowMore from "./components/KnowMore";
+import Predict from "./components/Predict";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-
 function App() {
   return (
     <>
       <Routes>
         <Route path="/know-more" element={<KnowMore />}></Route>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/predict" element={<Predict />}></Route>
       </Routes>
     </>
   );
